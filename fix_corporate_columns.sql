ea8b81698e66128ed1830aa01bdd7106e551e2c8
Malik <malik.s@allprimetech.com>
2026-06-03 10:12:29 +0100

    ok

---
+++ fix_corporate_columns.sql
@@ -0,0 +1,7 @@
+ALTER TABLE [TBL_CUSTOMER] ADD [NatureOfBusiness] nvarchar(250) NULL;
+ALTER TABLE [TBL_CUSTOMER] ADD [EntityType] nvarchar(50) NULL;
+ALTER TABLE [TBL_CUSTOMER] ADD [OtherEntityType] nvarchar(100) NULL;
+ALTER TABLE [TBL_CUSTOMER] ADD [AnnualTurnover] nvarchar(100) NULL;
+ALTER TABLE [TBL_CUSTOMER] ADD [SourceOfFunds] nvarchar(250) NULL;
+ALTER TABLE [TBL_CUSTOMER] ADD [ClientSegmentation] nvarchar(50) NULL;
+GO
